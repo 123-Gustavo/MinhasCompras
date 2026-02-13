934e4380a32d7845a7eb259f61b3c840d26f58ff
Gustavo <fariasgusgustavo@gmail.com>
2026-02-13 20:28:07 -0300

    Adicionar arquivos de projeto.

---
+++ MinhasCompras/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
