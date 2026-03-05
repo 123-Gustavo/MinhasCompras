c3171dea05da7580accd1637372b4fbe7c79733c
Gustavo <fariasgusgustavo@gmail.com>
2026-03-04 22:21:17 -0300

    Sistema para inserir no SQLite e tela de adicionar produto.

--- MinhasCompras/Views/NovoProduto.xaml.cs
+++ MinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,25 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = double.Parse(txt_quantidade.Text),
+                Preco = double.Parse(txt_preco.Text)
+            };
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso", "Produto cadastrado com sucesso!", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
 }
